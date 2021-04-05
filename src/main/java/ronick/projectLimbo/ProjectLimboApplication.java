3cc8d13d0b7ee80419edf2e90620f533b95ce9cc
ronick-grammer <glflakcm@gmail.com>
2021-04-05 11:32:20 +0900

    change for deploy

--- src/main/java/ronick/projectLimbo/ProjectLimboApplication.java
+++ src/main/java/ronick/projectLimbo/ProjectLimboApplication.java
@@ -1,5 +1,5 @@
 package ronick.projectLimbo;
-
+/*
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,4 +9,23 @@ public class ProjectLimboApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectLimboApplication.class, args);
 	}
+}
+*/
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//War
+@SpringBootApplication
+public class ProjectLimboApplication extends  SpringBootServletInitializer{
+
+    @Override
+    protected  SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(ProjectLimboApplication.class);
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(ProjectLimboApplication.class, args);
+    }
 }
